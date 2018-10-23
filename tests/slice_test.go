be37651b9260146af8b9980b7412b658e7d3f0ae
nextzhou <nextzhou@gmail.com>
2018-10-23 22:45:52 +0800

    [slice plugin] more unittest case

--- tests/slice_test.go
+++ tests/slice_test.go
@@ -4,6 +4,8 @@ import (
 	"json"
 	"testing"
 
+	"strconv"
+
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -114,6 +116,131 @@ func TestIntSlice(t *testing.T) {
 
 			s.InPlaceConcat(NewIntSliceFromSlice([]int{6, 7, 8}))
 			So(s.String(), ShouldEqual, "[1 2 3 4 5 6 7 8]")
+		})
+
+		Convey("for each", func() {
+			var s *IntSlice
+			sum := 0
+			s.ForEach(func(i int) { sum += i })
+			So(sum, ShouldEqual, 0)
+
+			s = NewIntSliceFromSlice([]int{1, 2, 3})
+			s.ForEach(func(i int) { sum += i })
+			So(sum, ShouldEqual, 6)
+
+			s.Clear()
+			s.ForEach(func(i int) { sum += i })
+			So(sum, ShouldEqual, 6)
+		})
+
+		Convey("index", func() {
+			ss := []int{1, 2, 3, 4, 5}
+			s := NewIntSliceFromSlice(ss)
+
+			for idx := range ss {
+				So(*s.Index(idx), ShouldEqual, ss[idx])
+			}
+
+			So(*s.Index(-1), ShouldEqual, 5)
+
+			*s.Index(0) = 100
+			So(s.String(), ShouldEqual, "[100 2 3 4 5]")
+
+			ss = s.IndexRange(1, -1)
+			So(NewIntSliceFromSlice(ss).String(), ShouldEqual, "[2 3 4]")
+
+			ss = s.IndexFrom(2)
+			So(NewIntSliceFromSlice(ss).String(), ShouldEqual, "[3 4 5]")
+
+			ss = s.IndexTo(3)
+			So(NewIntSliceFromSlice(ss).String(), ShouldEqual, "[100 2 3]")
+		})
+
+		Convey("find", func() {
+			s := NewIntSliceFromSlice([]int{1, 2, 3, 4, 5})
+			So(s.Find(100), ShouldEqual, -1)
+			So(s.FindBy(func(i int) bool { return true }), ShouldEqual, 0)
+			So(s.FindBy(func(i int) bool { return false }), ShouldEqual, -1)
+
+			So(s.Find(3), ShouldEqual, 2)
+			So(s.FindBy(func(i int) bool { return i%2 == 0 }), ShouldEqual, 1)
+
+			s.Append(4, 3, 2, 1)
+			So(s.Find(3), ShouldEqual, 2)
+			So(s.FindLast(3), ShouldEqual, 6)
+			So(s.FindBy(func(i int) bool { return i%2 == 0 }), ShouldEqual, 1)
+			So(s.FindLastBy(func(i int) bool { return i%2 == 0 }), ShouldEqual, 7)
+		})
+
+		Convey("count", func() {
+			var s *IntSlice
+			So(s.Count(1), ShouldEqual, 0)
+			So(s.CountBy(func(i int) bool { return true }), ShouldEqual, 0)
+			So(s.CountBy(func(i int) bool { return false }), ShouldEqual, 0)
+
+			s = NewIntSliceFromSlice([]int{1, 2, 3, 4, 5})
+			So(s.Count(0), ShouldEqual, 0)
+			So(s.Count(1), ShouldEqual, 1)
+			So(s.Count(2), ShouldEqual, 1)
+			s.Append(2)
+			So(s.Count(2), ShouldEqual, 2)
+
+			So(s.CountBy(func(i int) bool { return true }), ShouldEqual, s.Len())
+			So(s.CountBy(func(i int) bool { return false }), ShouldEqual, 0)
+			So(s.CountBy(func(i int) bool { return i%2 == 1 }), ShouldEqual, 3)
+		})
+
+		Convey("group", func() {
+			Convey("group by bool", func() {
+				s := NewIntSliceFromSlice([]int{1, 2, 3, 4, 5})
+				odd, even := s.GroupByBool(func(i int) bool { return i%2 == 1 })
+				So(odd.String(), ShouldEqual, "[1 3 5]")
+				So(even.String(), ShouldEqual, "[2 4]")
+
+				s = nil
+				odd, even = s.GroupByBool(func(i int) bool { return i%2 == 1 })
+				So(odd.IsEmpty(), ShouldBeTrue)
+				So(even.IsEmpty(), ShouldBeTrue)
+			})
+
+			Convey("group by int", func() {
+				s := NewIntSliceFromSlice([]int{1, 2, 3, 4, 5})
+				groups := s.GroupByInt(func(i int) int { return i % 3 })
+				So(groups[0].String(), ShouldEqual, "[3]")
+				So(groups[1].String(), ShouldEqual, "[1 4]")
+				So(groups[2].String(), ShouldEqual, "[2 5]")
+				So(groups[3].IsEmpty(), ShouldBeTrue)
+			})
+
+			Convey("group by str", func() {
+				s := NewIntSliceFromSlice([]int{1, 12, 2, 21, 22, 3})
+				groups := s.GroupByStr(func(i int) string {
+					return string(strconv.Itoa(i)[0])
+				})
+				So(groups[""].IsEmpty(), ShouldBeTrue)
+				So(groups["0"].IsEmpty(), ShouldBeTrue)
+				So(groups["12"].IsEmpty(), ShouldBeTrue)
+				So(groups["1"].String(), ShouldEqual, "[1 12]")
+				So(groups["2"].String(), ShouldEqual, "[2 21 22]")
+				So(groups["3"].String(), ShouldEqual, "[3]")
+			})
+
+			Convey("group by interface", func() {
+				s := NewIntSliceFromSlice([]int{1, 12, 2, 21, 22, 3})
+				groups := s.GroupBy(func(i int) interface{} {
+					s := strconv.Itoa(i)
+					if len(s) == 1 {
+						return i
+					}
+					return s[:1]
+				})
+				So(groups[1].String(), ShouldEqual, "[1]")
+				So(groups["1"].String(), ShouldEqual, "[12]")
+				So(groups[2].String(), ShouldEqual, "[2]")
+				So(groups["2"].String(), ShouldEqual, "[21 22]")
+				So(groups[3].String(), ShouldEqual, "[3]")
+				So(groups["3"].IsEmpty(), ShouldBeTrue)
+			})
 		})
 	})
 }
